f9b7c6e39ed4e2c8ded36cdc486ca153cc6f5682
이광훈 <leegh963@naver.com>
2024-07-09 12:22:49 +0900

    Fix : Builder 어노테이션 설정

--- src/main/java/com/example/docker/entity/Result.java
+++ src/main/java/com/example/docker/entity/Result.java
@@ -3,10 +3,16 @@ package com.example.docker.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Result {
 
     @Id
--- src/main/java/com/example/docker/entity/Story.java
+++ src/main/java/com/example/docker/entity/Story.java
@@ -3,11 +3,17 @@ package com.example.docker.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Story {
 
     @Id
