a94a70043b74a459a92ad4d4fb29acaf31ac75c1
이광훈 <leegh963@naver.com>
2024-10-06 11:31:33 +0900

    Fix : AuthInterceptor 에서는 session 의 존재여부만 확인

--- src/main/java/com/example/docker/interceptor/AuthInterceptor.java
+++ src/main/java/com/example/docker/interceptor/AuthInterceptor.java
@@ -3,17 +3,23 @@ package com.example.docker.interceptor;
 import com.example.docker.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
-@RequiredArgsConstructor
 public class AuthInterceptor implements HandlerInterceptor{
 
-    private final UserRepository userRepository;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        HttpSession session = request.getSession(false);
+
+        if(session == null){
+            throw new IllegalStateException("세션이 존재하지 않습니다");
+        }
+
         return true;
     }
 }
