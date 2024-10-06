0de97eb8821c5fcae7f2d49f16e3526c727ed8da
이광훈 <leegh963@naver.com>
2024-10-06 10:58:02 +0900

    Feat : userRepository DI 를 위해 Authintercepotor 에 component 추가

--- src/main/java/com/example/docker/interceptor/AuthInterceptor.java
+++ src/main/java/com/example/docker/interceptor/AuthInterceptor.java
@@ -1,9 +1,19 @@
 package com.example.docker.interceptor;
 
-
+import com.example.docker.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
-public class AuthInterceptor implements HandlerInterceptor {
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor{
+
+    private final UserRepository userRepository;
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
 }
